6ddc62e121380ff8625ab354687e9191f37bd10c
Ian <ian@greenit.cc>
2022-12-22 17:17:45 +0900

    test Entity

---
+++ cc/greenit/entity/Test.java
@@ -0,0 +1,42 @@
+package cc.greenit.entity;
+
+import org.hibernate.annotations.Comment;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Table(name = "REFACTOR_TEST", schema = "REFACTOR")
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Test extends BaseDatetimeExtended implements Serializable {
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    @Column(name = "ID", updatable = false, length = 40)
+    @Comment("아이디")
+    private String id;
+
+    @Column(name = "TEST_NAME", length = 40)
+    @Comment("이름")
+    private String testName;
+}
